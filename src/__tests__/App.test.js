98a57476ef1bb4ae0ea307f5adbac7eea0c06f75
Sara Cook <saracook087@gmail.com>
2022-07-18 15:27:29 +0100

    initial commit

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  expect(screen.getByRole("heading", {level: 2})).toHaveTextContent("Surreal Estate");
+});
