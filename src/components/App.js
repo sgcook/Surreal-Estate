cfee28a20964a0d473b524ea331619d936aae660
Sara Cook <saracook087@gmail.com>
2022-07-27 22:39:45 +0100

    added FacebookLogin

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import "../styles/app.css";
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./NavBar";
 import Properties from "./Properties";
 import AddProperty from "./AddProperty";
 
 const App = () => {
+  const [userId, setUserId] = useState("");
+
+  const handleLogin = (response) => {
+    setUserId(response.userID);
+  };
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    window.FB.logout(function () {
+      setUserId(null);
+    });
+  };
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar onLogin={handleLogin} userID={userId} onLogout={handleLogout} />
 
       <Routes>
         <Route path="/view-properties" element={<Properties />} />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../styles/navbar.css";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import FacebookLogin from "react-facebook-login";
 import image from "../surrealEstateLogo.png";
 
-const Navbar = () => {
+const Navbar = ({ onLogin, onLogout, userID }) => {
   return (
     <div className="navbar">
       <img className="surrealEstateLogo" src={image} alt="surrealEstateLogo" />
@@ -14,9 +16,28 @@ const Navbar = () => {
         <Link className="navbar-links-item" to="/add-property">
           Add Property
         </Link>
+        {userID ? (
+          <button type="button" onClick={onLogout}>
+            Sign Out
+          </button>
+        ) : (
+          <FacebookLogin callback={onLogin} appId="774246670282431" />
+        )}
       </ul>
     </div>
   );
 };
 
 export default Navbar;
+
+Navbar.propTypes = {
+  onLogin: PropTypes.func,
+  onLogout: PropTypes.func,
+  userID: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  onLogin: () => {},
+  onLogout: () => {},
+  userID: "",
+};
